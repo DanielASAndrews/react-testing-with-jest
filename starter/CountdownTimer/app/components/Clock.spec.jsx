7e4d720ad96708c97e5a4554ef7b3ea1f3de4daf
Daniel Andrews <daniel.a.s.andrews@gmail.com>
2018-02-04 17:47:42 -0500

    make enzyme imports global

--- starter/CountdownTimer/app/components/Clock.spec.jsx
+++ starter/CountdownTimer/app/components/Clock.spec.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
 import Clock from 'Clock';
 
 describe('Clock', () => {
--- starter/CountdownTimer/app/components/CountdownForm.spec.jsx
+++ starter/CountdownTimer/app/components/CountdownForm.spec.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
 import CountdownForm from 'CountdownForm';
 
 describe('CountdownForm', ()=> {
