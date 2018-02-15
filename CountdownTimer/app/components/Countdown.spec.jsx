f275cfdd5f1c392c21a681a78ebe8ad1201f6530
Daniel Andrews <daniel.a.s.andrews@gmail.com>
2018-02-14 22:32:47 -0500

    add child component render tests

--- CountdownTimer/app/components/Countdown.spec.jsx
+++ CountdownTimer/app/components/Countdown.spec.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Countdown from 'Countdown';
+import Clock from 'Clock';
+import CountdownForm from 'CountdownForm';
 
 jest.useFakeTimers();
 
@@ -7,17 +9,17 @@ describe('Countdown', () => {
 
     let countDown;
 
-    beforeEach(()=>{
+    beforeEach(() => {
         countDown = mount(<Countdown />);
     });
 
-    afterEach(()=>{
+    afterEach(() => {
         countDown.unmount();
     });
 
     describe('handleSetCountdownTime()', () => {
         it('it should set countdown time and start countdown', () => {
-            
+
             const actual = countDown.instance().handleSetCountdownTime(10);
             const expected = { count: 10, countdownStatus: 1 };
 
@@ -30,12 +32,24 @@ describe('Countdown', () => {
         });
 
         it('should never set countdown time to less than zero', () => {
-            
+
             const actual = countDown.instance().handleSetCountdownTime(1);
 
             jest.advanceTimersByTime(3000);
 
             expect(countDown.state().count).toEqual(0);
+        });
+    });
+
+    describe('render', () => {
+        it('should always render Clock', () => {
+            expect(countDown.find(Clock).length).toEqual(1);
+        });
+    });
+
+    describe('render', () => {
+        it('should always render CountdownForm', () => {
+            expect(countDown.find(CountdownForm).length).toEqual(1);
         });
     });
 });
